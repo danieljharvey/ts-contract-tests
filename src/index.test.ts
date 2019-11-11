372865d97317fd3c3fd39d96be0064bfde8b4aa5
Daniel Harvey <danieljamesharvey@gmail.com>
2019-11-11 23:22:55 +0000

    Actually create samples

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,14 +3,16 @@ import { getArbitrary } from 'fast-check-io-ts';
 import { either } from 'fp-ts'
 
 import { User, UserShape } from './index'
+import { generate } from './generate'
 
 const userArbitrary = getArbitrary(UserShape)
 
 describe('User', () => {
   test('Round trip encode and decode', () => {
-    fc.assert(fc.property(userArbitrary, 
+    fc.assert(fc.property(userArbitrary,
       (user: User) => either.isRight(UserShape.decode(user))
     ))
+    generate(userArbitrary)
   })
 })
 
