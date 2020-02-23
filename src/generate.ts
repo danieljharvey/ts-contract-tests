fdaf8b4cca91e80d9491ee307aa79a1410ac67b5
Daniel Harvey <danieljamesharvey@gmail.com>
2020-02-23 16:39:01 +0000

    Save files and read them

--- src/generate.ts
+++ src/generate.ts
@@ -2,12 +2,9 @@ import * as fc from 'fast-check'
 import * as fs from 'fs'
 import * as path from 'path'
 
-const outputPath = "./temp-output/"
-
-export const generate = <A>(arb: fc.Arbitrary<A>) => {
+export const generate = <A>(outputPath: string, arb: fc.Arbitrary<A>) => {
   fc.sample(arb, 100).map(a => JSON.stringify(a)).map((json, index) => {
     const filename = path.resolve(`${outputPath}${index}.json`)
-    console.log(filename)
     fs.writeFileSync(filename, json)
   })
 }
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,19 +1,31 @@
 const fc = require('fast-check');
 import { getArbitrary } from 'fast-check-io-ts';
 import { either } from 'fp-ts'
+import * as path from 'path'
+import * as fs from 'fs'
 
 import { User, UserShape } from './index'
 import { generate } from './generate'
 
 const userArbitrary = getArbitrary(UserShape)
 
+const rangeArr = [...Array(100).keys()];
+
+const outputPath = './temp-output/'
+
 describe('User', () => {
   test('Round trip encode and decode', () => {
     fc.assert(fc.property(userArbitrary,
       (user: User) => either.isRight(UserShape.decode(user))
     ))
-    generate(userArbitrary)
+  })
+  test('Write to and read from files', () => {
+    generate(outputPath, userArbitrary)
+    rangeArr.forEach(index => {
+      const filename = path.resolve(`${outputPath}${index}.json`)
+      const file = JSON.parse(fs.readFileSync(filename, "utf8"))
+      expect(either.isRight(UserShape.decode(file))).toBeTruthy()
+    })
   })
 })
-
 
